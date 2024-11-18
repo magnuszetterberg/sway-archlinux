bd1d8ca8729ad0969a3d08e80fcec0c1c5efa470
Magnus Zetterberg <magnus.zetterberg>
2024-11-18 19:35:07 +0100

    minor updates

--- dotfiles/waybar/toggleaudio.sh
+++ dotfiles/waybar/toggleaudio.sh
@@ -4,7 +4,7 @@ newSink=""
 
 #The alsa_out... string is hardcoded to your soundoutputs... please update them for your setup
 Headphones="alsa_output.usb-GN_Netcom_A_S_Jabra_EVOLVE_LINK_MS_0006FC45D50A08-00.analog-stereo" #Change this device's to headphone pactl name
-Speaker="alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi__Speaker__sink" #Change this to device's speaker pactl name
+Speaker="alsa_output.pci-0000_00_1f.3-platform-tgl_rt5682_def.pro-output-0" #Change this to device's speaker pactl name
 currentSink=$(pactl info | sed -n 's/Default Sink: //p')
 
 if [ "$Speaker" = "$currentSink" ]; then
